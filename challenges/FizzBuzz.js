c41fae4e8f67920ff5a5a010dddf8ed36f1d5356
Allan Camilo <acamilo.ag@gmail.com>
2019-12-09 14:22:19 +0200

    Added feature to  build a grid of any size

--- challenges/FizzBuzz.js
+++ challenges/FizzBuzz.js
@@ -1,32 +1,42 @@
 let chooseChar = '#';
 let space = ' ';
-let gridLenth = 8;
+let gridLenth = 3;
 
 function printGrid(chooseChar, gridLenth, sperator) {
-    let line = '';
+    let grid = '';
     let counter = 0;
     const extendedGridLength = gridLenth * gridLenth + (gridLenth - 1);
     
     for(i = 0; i < extendedGridLength; i++) {
-        if(!(i % 2)) {
-            line = line + sperator; 
-        }
-        else if (i % 2) {
-            line = line + chooseChar; 
-        }
 
-        ++counter;        
+        if(!(i % 2))
+            grid = grid + sperator; 
+        else if (i % 2)
+            grid = grid + chooseChar;
+
+        ++counter; 
 
         if(counter === gridLenth) {
-            line = line + '\n';
-            i++;
+            grid = grid + '\n';
+            i++;                      
             counter = 0;
+
+            //rearange chars if the grid length is not a pair number
+            if(gridLenth % 2) {
+                const aux = sperator;
+                sperator = chooseChar;
+                chooseChar = aux;
+            }
         }
-
     }
-
-    console.log(line);
+    console.log(grid);
 }
+
+/*function exchangeValues(value1, value2) {
+    const auxValueHandler = value1;
+    value1 = value2;
+    value2 = auxValueHandler;    
+}*/
 
 printGrid(chooseChar, gridLenth, space);
 
