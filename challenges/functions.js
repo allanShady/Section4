89807c03ed46b94a7e1dd3251de1a324ea471e95
Allan Camilo <acamilo.ag@gmail.com>
2019-12-10 16:07:25 +0200

    Added function to count the number of ocurrence of a specific given character.

--- challenges/functions.js
+++ challenges/functions.js
@@ -8,6 +8,23 @@ const isEven = (number) => {
     return isEven(number - 2);
 }
 
+const countBS = (text) => {   
+    return countChar(text, 'B');
+}
+
+const countChar = (text, charToCount) => {
+    let charOcurrence = 0;
+
+    for(i = 0; i < text.length; i++) {
+        if(text[i] === charToCount)
+            charOcurrence++;
+    }
+    
+    return charOcurrence;
+}
+
+const str = 'Best fied for ever bff BBB'
+console.log(`The char B apper in ${str} ${countBS(str)}`);
 //Is even
 console.log(`is even? [${isEven(7)}]`);
 //Determine min value
