0669a1cdfbd6dc60bcb357d5c2bb8eb232aa32bc
Allan Camilo <acamilo.ag@gmail.com>
2019-12-15 10:52:35 +0200

    Added is even function and call sample

--- challenges/HighOrderFunction/highof.js
+++ challenges/HighOrderFunction/highof.js
@@ -4,5 +4,13 @@ function repeat(n, action) {
     }
 }
 
+function unless(test, then) { 
+    if (!test) then(); 
+}
+
+console.log(`Repeat console.log`)
 repeat(5, console.log);
+console.log('Call: repeat(3, i => { console.log(isNaN(i), i) })');
 repeat(3, i => { console.log(isNaN(i), i) });
+console.log(`Call: repeat(3, n => { unless(n % 2 == 1, () => { console.log(n, 'is even'); }); })`);
+repeat(3, n => { unless(n % 2 == 1, () => { console.log(n, 'is even'); }); });
