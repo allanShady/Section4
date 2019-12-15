ac4f8ca9e736b82b4f5ff2e9e2652255cfb388fd
Allan Camilo <acamilo.ag@gmail.com>
2019-12-15 10:46:00 +0200

    HOF testes

--- challenges/HighOrderFunction/highof.js
+++ challenges/HighOrderFunction/highof.js
@@ -1,6 +1,8 @@
-function repeat(n, ction) { 
+function repeat(n, action) { 
     for (i = 0; i < n; i++) { 
         action(i); 
     }
 }
 
+repeat(5, console.log);
+repeat(3, i => { console.log(isNaN(i), i) });
