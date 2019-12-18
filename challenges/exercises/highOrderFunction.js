bd7889d974187376012b00dd2b0e7ea4e2cceffc
Allan Camilo <acamilo.ag@gmail.com>
2019-12-18 08:24:34 +0200

    Adds the 'Your own loop' exercise

--- challenges/exercises/highOrderFunction.js
+++ challenges/exercises/highOrderFunction.js
@@ -1,13 +1,23 @@
 const flatteningBtn = document.querySelector('#flattening');
+const ownloopBtn = document.querySelector('#ownloop');
 
 const flattenArray = array => {
     return array.reduce((b, c) => b.concat(c));
 }
 
 function flatten() {
-    
-    document.querySelector('.show').style.visibility = 'hidden';
+    hideHint('#flatteninghint');
     console.log(flattenArray([[1, 2, 3,], [4, 5, 6], [7, 8, 9, 90, 89, 'l']]));
 }
 
+function hideHint(selector) {
+    document.querySelector(selector).style.visibility = 'hidden';
+}
+
+const loopfunc = () => {
+    hideHint('#ownloophint');
+    console.log('Loop clicked');
+}
+
 flatteningBtn.addEventListener('click', flatten);
+ownloopBtn.addEventListener('click', loopfunc);
